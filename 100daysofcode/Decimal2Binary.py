84877339eeb2057389ee2d35f709f52614939833
Jon Lickey <jlickey@cisco.com>
2022-04-07 13:03:36 -0700

    Removed Folder

--- 100daysofcode/Decimal2Binary.py
+++
@@ -1,9 +0,0 @@
-#!/bin/env python -tt
-
-def convert2binary(number):
-    binary_number = bin(number).replace("0b", "")
-    print(binary_number) 
-
-if __name__ == "__main__":
-    number = int(input("Enter number to convert to binary: "))
-    convert2binary(number)
--- GenPasswd/README.md
+++
@@ -1,7 +0,0 @@
-# GenPasswd
- 
-![image](https://user-images.githubusercontent.com/13524582/156695095-28a2abb6-f413-48d6-a4e3-b4f28c946aeb.png)
-
-Used PyInstaller to create .exe file
-
-https://www.geeksforgeeks.org/convert-python-script-to-exe-file/
