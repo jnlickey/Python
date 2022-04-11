8cdb5afe95be6c6c52be9a44c8b55275522a9c57
Jon Lickey <jlickey@cisco.com>
2022-04-11 06:29:07 -0700

    Removed Folder

--- AD_Migration/MSADsetup/JoinAD.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3 -tt
-import subprocess
-import pexpect
-
-child = pexpect.spawn ('realm join -U Administrator ad.cll.cloud --computer-name="SJ-ADTEST-DEV-01" --os-name="Ubuntu" --os-version="Ubuntu 20.04.3 LTS" --computer-ou="CN=Computers,DC=ad,DC=cll,DC=cloud" --automatic-id-mapping=no')
-child.expect ('Password for Administrator:')
-child.sendline ("<passwd_removed>")
-child.expect(pexpect.EOF)
-print('Joining ad.cll.cloud domain')
