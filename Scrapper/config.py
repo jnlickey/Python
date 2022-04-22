18a560470824f34f63630547bf15ab51703bbb80
Jon Lickey <jlickey@cisco.com>
2022-04-22 08:14:05 -0700

    Scrapper

---
+++ Scrapper/config.py
@@ -0,0 +1,3 @@
+#!/bin/env python -tt
+user = str("jlickey")
+passwd = str("L1zz1e33!L1zz1e33")
---
+++ Scrapper/scrapper.py
@@ -0,0 +1,28 @@
+#!/bin/env python -tt
+import config
+import urllib.request
+
+user = config.user
+passwd = config.passwd
+
+url = 'http://172.16.50.5/remotes'
+
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+
+#headers = urllib3.make_headers(basic_auth='{user}:{passwd}')
+#r = urllib3.request('GET', url, headers=headers)
+#page = urlopen(r)
+#print(page)
+
+
+
+#url = "http://172.16.50.5/remotes"
+#http = urllib3.PoolManager()
+#headers = urllib3.make_headers(basic_auth='{user}:{passwd}')
+#r = http.request('GET', url, headers=headers)
+#page = urlopen(r)
+
+#html_bytes = page.read()
+#html = html_bytes.decode("utf-8")
+#print(html)
